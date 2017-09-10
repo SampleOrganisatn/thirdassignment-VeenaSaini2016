c7f8d6124b3eb7a4925df536f60aa2349c07bd0c
Apexit <Apexit@LAPTOP-0MD6A2N0>
2017-09-10 23:31:23 +0530

    this is sample assignment

--- SampleFirst/src/Sample.java
+++ SampleFirst/src/Sample.java
@@ -1,8 +1,45 @@
+/**
+ * <h1>This one is the first heading</h1>
+ * <p>helllo there this is one of the best program i have written in this class.</p>
+ * <p>helllo there this is one of the second best program i have written in this class.</p>
+ * @author Apexit
+ * @version 10.1
+ * @since 2017-08-25
+ 
+*/
 
 public class Sample {
 
+	/**
+	 * @return int 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("hello there");
+	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @param b
+	 * @return 0
+	 * @see <a href="https://www.youtube.com/">link</a>"this link"
+	 */
+	
+	public static int anothermethod(int n, int b)
+	{
+		//sample method second
+		return n;
+	}
+	
+	/**
+	 * 
+	 * @param a
+	 * @return 
+	 */
+	public int fun(int a)
+	{
+		return 0;
 	}
 }
